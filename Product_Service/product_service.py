960872b750cee0b47276fb8e4ef5220c50e58223
Tjielke <t.p.j.nabuurs@student.tue.nl>
2024-05-20 12:31:58 +0200

    Trying new routing

--- Product_Service/product_service.py
+++ Product_Service/product_service.py
@@ -27,4 +27,4 @@ def delete_delivery(d_id):
     return Product.delete(d_id)
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 5001)), host='0.0.0.0')
+    app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0')
