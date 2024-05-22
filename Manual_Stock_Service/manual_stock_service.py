e3cf0e522cad332341abf0f1b4fbb7422232004b
Tjielke <t.p.j.nabuurs@student.tue.nl>
2024-05-22 22:53:55 +0200

    fixing nonetype

--- Manual_Stock_Service/manual_stock_service.py
+++ Manual_Stock_Service/manual_stock_service.py
@@ -12,7 +12,7 @@ Base.metadata.create_all(engine)
 
 @app.route('/product/<d_id>/stock', methods=['PUT'])
 def update_delivery_status(d_id):
-    stock = request.args.get('stock')
+    stock = request.get_json()
     return Stock.update(d_id, stock)
 
 if __name__ == '__main__':
