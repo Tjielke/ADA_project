3aab0a27b8ee933672a07aab949e696d586966d4
Tjielke <t.p.j.nabuurs@student.tue.nl>
2024-05-22 22:48:30 +0200

    zelfde errors doorlopen

--- Manual_Stock_Service/resources/stock.py
+++ Manual_Stock_Service/resources/stock.py
@@ -13,6 +13,6 @@ class Stock:
         session = Session()
         Product = session.query(Product_DAO).filter(Product_DAO.id == int(d_id))[0]
         Product.stock.stock = new_stock
-        Product.stock.last_update = datetime.datetime.now()
+        Product.stock.last_update = datetime.now()
         session.commit()
         return jsonify({'message': 'The stock position was updated'}), 200
